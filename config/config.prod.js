9373b86a98c83939c2605acd600a451d970f2ccd
keno <348605879@qq.com>
2020-03-23 22:48:17 +0800

    修改config

---
+++ config/config.prod.js
@@ -0,0 +1,29 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+require('dotenv').config();
+const path = require('path');
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || '3306',
+    database: 'eshop',
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '12345678',
+  };
+
+  return {
+    ...config,
+  };
+};
